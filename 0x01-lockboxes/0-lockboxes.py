362633bb719fc7cf9a87cb71838007a0ecc35af0
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-01-31 08:44:50 +0100

    Task done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,7 +14,6 @@ def canUnlockAll(boxes):
     """
     opened_boxes = set()
     opened_boxes.add(0)
-    
     stack = [0]
     while stack:
         box = stack.pop()
@@ -22,5 +21,4 @@ def canUnlockAll(boxes):
             if key not in opened_boxes:
                 opened_boxes.add(key)
                 stack.append(key)
-    
     return len(opened_boxes) == len(boxes)
