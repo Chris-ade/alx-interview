864a236e8ba261971081df6360055f7e2240d767
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-02-22 21:32:40 +0100

    Task done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,25 +6,9 @@ UTF-8 Valdation Module
 
 def validUTF8(data):
     """ Determines if a given data set represents a valid UTF-8 encoding """
-    num_bytes = 0
-    for i in data:
-        # Check if the current byte is a start of a new character
-        if num_bytes == 0:
-            # Determine the number of bytes in the current character
-            if (i >> 5) == 0b110:
-                num_bytes = 1
-            elif (i >> 4) == 0b1110:
-                num_bytes = 2
-            elif (i >> 3) == 0b11110:
-                num_bytes = 3
-            elif (i >> 7) == 0b0:
-                num_bytes = 0
-            else:
-                # Current byte is a continuation byte or an invalid UTF-8 char
-                return False
-        else:
-            # Check if the current byte is a continuation byte
-            if (i >> 6) != 0b10:
-                return False
-            num_bytes -= 1
-    return num_bytes == 0
+    try:
+        maskeddata = [n & 255 for n in data]
+        bytes(maskeddata).decode("UTF-8", errors="strict")
+        return True
+    except UnicodeDecodeError:
+        return False
