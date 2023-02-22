16e5919fcda8b0ad8ed13816490943b28aa4c5c3
Chris-ade <joeade12054@gmail.com>
2023-02-22 12:12:10 -0800

    Task done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,33 +1,23 @@
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
     num_bytes = 0
-
-    # Loop through all integers in the data set
-    for num in data:
-        # If this is the start of a new UTF-8 character
+    for i in data:
+        # Check if the current byte is a start of a new character
         if num_bytes == 0:
-            # Determine the number of bytes in this UTF-8 character
-            if num >> 7 == 0b0:
-                # 1-byte UTF-8 character
+            # Determine the number of bytes in the current character
+            if (i >> 5) == 0b110:
                 num_bytes = 1
-            elif num >> 5 == 0b110:
-                # 2-byte UTF-8 character
+            elif (i >> 4) == 0b1110:
                 num_bytes = 2
-            elif num >> 4 == 0b1110:
-                # 3-byte UTF-8 character
+            elif (i >> 3) == 0b11110:
                 num_bytes = 3
-            elif num >> 3 == 0b11110:
-                # 4-byte UTF-8 character
-                num_bytes = 4
+            elif (i >> 7) == 0b0:
+                num_bytes = 0
             else:
-                # Invalid UTF-8 character
+                # Current byte is a continuation byte or an invalid UTF-8 character
                 return False
         else:
-            # This is a continuation byte
-            if num >> 6 != 0b10:
-                # Invalid continuation byte
+            # Check if the current byte is a continuation byte
+            if (i >> 6) != 0b10:
                 return False
-            # Decrement the number of bytes left in the current character
             num_bytes -= 1
-    # If there are no more continuation bytes expected, then the data set is valid
     return num_bytes == 0
