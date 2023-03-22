5e4d3a1d577e244b1e9a8bde7d61078bcf944e68
Chris Adebiyi <101924502+Chris-ade@users.noreply.github.com>
2023-03-22 19:37:16 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+Making changes: module 
+"""
+
 
 def makeChange(coins, total):
+    """ 
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    """
     if total <= 0:
         return 0
 
